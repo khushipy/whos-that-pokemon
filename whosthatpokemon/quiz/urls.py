80e824a005ab740a5ea1a629644711dd0d9a1db9
Khushi <Khushipaltwt@gmail.com>
2025-12-12 21:47:58 +0530

    wrong push

--- whosthatpokemon/quiz/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.home, name="home"),
-    path("quiz/", views.quiz_page, name="quiz"),
-    path("submit/", views.submit_quiz, name="submit"),
-]
